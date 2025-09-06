4ca4ee59e1a8feb9445f34410ba55729ef04d67a
Victor Yohanna <you@example.com>
2025-09-06 10:22:25 +0100

    clear console logs

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/", auth);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    //console.error(err.stack);
+   
     res.status(500).json({
         success: false,
         error: 'Server Error'
